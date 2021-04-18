b77ef593735c33ec5681e9dc9ce067710141f095
David Cole <david.cole1340@gmail.com>
2021-04-18 23:32:19 +1200

    Added encode and bitrate functions

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,4 +1,4 @@
-use audiopus::{coder::Encoder, Application, Channels, SampleRate, TryFrom};
+use audiopus::{coder::Encoder, Application, Bitrate, Channels, SampleRate, TryFrom};
 use ext_php_rs::{
     parse_args,
     php::{
@@ -17,6 +17,8 @@ use ext_php_rs::{
 #[derive(Default, Debug, ZendObjectHandler)]
 pub struct OpusEncoder {
     encoder: Option<Encoder>,
+    sample_rate: ZendLong,
+    channels: ZendLong,
 }
 
 impl OpusEncoder {
@@ -28,8 +30,20 @@ impl OpusEncoder {
             .arg(Arg::new("application", DataType::String))
             .build();
 
+        let encode = FunctionBuilder::new("encode", OpusEncoder::encode)
+            .arg(Arg::new("data", DataType::Array))
+            .arg(Arg::new("frame_size", DataType::Long))
+            .returns(DataType::Array, false, false)
+            .build();
+
+        let set_bitrate = FunctionBuilder::new("setBitrate", OpusEncoder::set_bitrate)
+            .arg(Arg::new("bitrate", DataType::Long))
+            .build();
+
         ClassBuilder::new("OpusEncoder")
             .method(constructor, MethodFlags::Public)
+            .method(encode, MethodFlags::Public)
+            .method(set_bitrate, MethodFlags::Public)
             .object_override::<OpusEncoder>()
             .build();
     }
@@ -73,5 +87,63 @@ impl OpusEncoder {
         let this = ZendClassObject::<Self>::get(execute_data).unwrap();
         let encoder = Encoder::new(sample_rate, channels, application).unwrap();
         this.encoder = Some(encoder);
+        this.sample_rate = sample_rate as _;
+        this.channels = channels as _;
+    }
+
+    pub extern "C" fn set_bitrate(execute_data: &mut ExecutionData, _: &mut Zval) {
+        let mut bitrate = Arg::new("bitrate", DataType::Long);
+        parse_args!(execute_data, bitrate);
+
+        let bitrate = match Bitrate::try_from(bitrate.val::<ZendLong>().unwrap() as _) {
+            Ok(x) => x,
+            Err(_) => {
+                exceptions::throw(ClassEntry::parse_error(), "Invalid bitrate given.");
+                return;
+            }
+        };
+
+        let this = ZendClassObject::<Self>::get(execute_data).unwrap();
+
+        if let Err(e) = this.encoder.as_mut().unwrap().set_bitrate(bitrate) {
+            exceptions::throw(
+                ClassEntry::error_exception(),
+                format!(
+                    "Error setting the bitrate of the encoder: {}",
+                    e.to_string()
+                )
+                .as_str(),
+            )
+        }
+    }
+
+    /// Encodes an array of Opus data.
+    pub extern "C" fn encode(execute_data: &mut ExecutionData, retval: &mut Zval) {
+        let mut data = Arg::new("data", DataType::Array);
+        let mut frame_size = Arg::new("frame_size", DataType::Long);
+        parse_args!(execute_data, data, frame_size);
+
+        let this = ZendClassObject::<Self>::get(execute_data).unwrap();
+        let arr = data.zval().unwrap().array().unwrap();
+        let pcm: Vec<ZendLong> = (&arr).into();
+        let mut output =
+            Vec::with_capacity((frame_size.val::<ZendLong>().unwrap() * this.channels * 2) as _);
+
+        let result = this
+            .encoder
+            .as_ref()
+            .unwrap()
+            .encode(&[1, 2, 3, 4], &mut output);
+
+        if result.is_err() {
+            let err = result.unwrap_err().to_string();
+            exceptions::throw(
+                ClassEntry::error_exception(),
+                format!("Error encoding PCM to Opus: {}", err).as_str(),
+            );
+            return;
+        }
+
+        retval.set_array(output);
     }
 }
